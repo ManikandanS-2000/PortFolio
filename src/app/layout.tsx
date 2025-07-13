bff1eb42c22517d013dd11f7f7cfda4f480fff64
Manikandan S <manikandan.s161220@gmail.com>
2025-07-13 22:06:14 +0530

    Introducing Static Nav bar, content section with about and skills

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Funnel_Display } from "next/font/google";
 import "./globals.css";
+import SplashCursor , {ColorRGB} from '@/components/cursor/cursor';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = Funnel_Display({
+  variable: "--font-funal-display",
   subsets: ["latin"],
 });
 
@@ -25,8 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`w-dvw h-dvh ${font.className} antialiased`}
       >
+        {/* <SplashCursor/> */}
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
-
+import NavBar from "@/components/navBar/navBar"
+import Content from "@/components/content/content"
+import Image from "next/image"
 export default function Home() {
   return (
-    <div className="">
-    </div>
-  );
+  <div className="size-full relative z-[60] px-30 py-3 flex flex-col gap-3">
+    <NavBar></NavBar>
+    <Content></Content>
+  </div>
+  )
 }
